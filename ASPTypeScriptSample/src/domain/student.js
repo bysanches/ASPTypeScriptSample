d11404c235e8a41fce66a94c45ed05945492b25e
Bruno Yuji Sanches Aoyagi <bruno.aoyagi@thyssenkrupp.com>
2017-07-31 20:17:09 -0300

    TypeScript output removed.

--- ASPTypeScriptSample/src/domain/student.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=student.js.map
--- ASPTypeScriptSample/src/helpers/handlebars-helpers.js
+++
@@ -1,7 +0,0 @@
-Handlebars.registerHelper('formatDate', function (d) {
-    if (d != null && moment(d).isValid()) {
-        return moment(d).format('DD/MM/YYYY');
-    }
-    return d;
-});
-//# sourceMappingURL=handlebars-helpers.js.map
